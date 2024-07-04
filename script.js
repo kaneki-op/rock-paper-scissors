8fede96b7790db26e2772e37670dce71377092bc
Prakash <85176794+kaneki-op@users.noreply.github.com>
2024-07-04 18:27:22 +0530

    Create getHumanChoice function which asks for the user the choice and returns the value based on that

--- script.js
+++ script.js
@@ -19,4 +19,29 @@ function getComputerChoice(max) {
         alert("scissors")
     }
 }
-getComputerChoice(3)
+//getComputerChoice(3)
+
+//creating getHumanChoice function 
+// asking user the choice 
+// and returning based on their choice
+
+function getHumanChoice() {
+    choice = Number(prompt("which one would you like to choose \n 1.rock 2.paper 3.scissors",""));
+    console.log(choice)
+    if (choice==1) {
+        choice = 'rock';
+        console.log(choice);
+    }
+    else if (choice==2) {
+        choice = 'paper';
+        console.log(choice);
+    }
+    else if (choice==3) {
+        choice = 'scissors';
+        console.log(choice);
+    }
+    else {
+        alert("Please Enter from the choice")
+    }
+}
+getHumanChoice()
